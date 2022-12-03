1d877d1b515934131102acf7f7b9eb9ad81ea3c5
Priyanka <priyankasharma.prisha@gmail.com>
2022-12-03 17:47:22 +0530

    set up router

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import Home from "./pages/Home";
 import Reports from "./pages/Reports";
 
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
@@ -12,21 +12,12 @@ function App() {
       <Sidebar />
       <div className="col-sm-12">
         <div className="container">
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/reports" element={<Reports />} />
+            </Routes>
+          </Router>
         </div>
       </div>
     </div>

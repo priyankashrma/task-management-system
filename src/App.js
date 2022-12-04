27dc0f2a14d71e3ddd5c46f46ecfbc6c31ebf871
Priyanka <priyankasharma.prisha@gmail.com>
2022-12-05 03:19:08 +0530

    Router changes and context declaration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ls from "local-storage";
+
 import "./App.css";
 import Home from "./pages/Home";
 import Reports from "./pages/Reports";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import Sidebar from "./components/Sidebar";
+import Login from "./pages/Login";
+import TaskContextProvider from "./context/TaskContext";
 
 function App() {
+  const loggedInUser = ls.get("loggedInUser");
   return (
     <div className="row">
       <Sidebar />
-      <div className="col-sm-12">
-        <div className="container">
-          <Router>
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/reports" element={<Reports />} />
-            </Routes>
-          </Router>
+
+      <TaskContextProvider>
+        <div className="col-sm-12">
+          <div className="container">
+            <Router>
+              <Routes>
+                <Route
+                  exact
+                  path="/"
+                  element={loggedInUser ? <Home /> : <Login />}
+                />
+                <Route
+                  path="/tasks"
+                  element={loggedInUser ? <Home /> : <Login />}
+                />
+                <Route
+                  path="/reports"
+                  element={loggedInUser ? <Reports /> : <Login />}
+                />
+              </Routes>
+            </Router>
+          </div>
         </div>
-      </div>
+      </TaskContextProvider>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,45 +1,41 @@
-import React from "react";
-import { Table, Form } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Table, Form, Alert } from "react-bootstrap";
+import { TaskContext } from "../context/TaskContext";
 
 const Tasks = () => {
+  const { tasks, updateStatus } = useContext(TaskContext);
   return (
-    <div className="tasks">
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Serial No.</th>
-            <th>Task</th>
-            <th>Due Date</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Do Laundry</td>
-            <td>2.12.2022</td>
-            <td>
-              <Form.Check type="checkbox" />
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Do Laundry</td>
-            <td>2.12.2022</td>
-            <td>
-              <Form.Check type="checkbox" />
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Do Laundry</td>
-            <td>2.12.2022</td>
-            <td>
-              <Form.Check type="checkbox" />
-            </td>
-          </tr>
-        </tbody>
-      </Table>
+    <div className="tasks overflow-scroll">
+      {tasks.length ? (
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Serial No.</th>
+              <th>Task</th>
+              <th>Due Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <tr key={task.title + task.id}>
+                <td>{task.id}</td>
+                <td>{task.title}</td>
+                <td>{task.dueDate}</td>
+                <td>
+                  <Form.Check
+                    type="checkbox"
+                    checked={task.status}
+                    onChange={() => updateStatus(task.id)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <Alert type="primary">No Tasks yet.</Alert>
+      )}
     </div>
   );
 };

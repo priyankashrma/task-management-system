e7627c4cbad2e6bd91891276364556497298b08d
Priyanka <priyankasharma.prisha@gmail.com>
2022-12-05 03:18:04 +0530

    Login Functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,101 @@
-import React from "react";
+import ls from "local-storage";
+import React, { useContext, useState } from "react";
+
+import { useNavigate } from "react-router-dom";
 import { Button, Form, Container, Alert } from "react-bootstrap";
+import { TaskContext } from "../context/TaskContext";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const { setTasks } = useContext(TaskContext);
+
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [disabledLogin, setDisabledLogin] = useState(false);
+
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const users = JSON.parse(ls.get("usersTaskHuman")) || [];
+
+  const login = (e) => {
+    if (users.length) {
+      const filteredUsers = users.filter(
+        (user) => user.email === formState.email
+      );
+
+      if (filteredUsers.length) {
+        const filteredUser = filteredUsers[0];
+
+        if (formState.password === filteredUser.password) {
+          setDisabledLogin(true);
+
+          setMessage("Logged In Successfully");
+          ls("loggedInUser", formState.email);
+          setTasks(filteredUser.tasks);
+          setTimeout(() => {
+            navigate("/tasks");
+          }, 500);
+        } else {
+          setError("Password does not match");
+          setFormState({ ...formState, password: "" });
+        }
+      } else {
+        createUser();
+      }
+    } else {
+      createUser();
+    }
+  };
+
+  const createUser = () => {
+    const newUser = {
+      user_id: users.length + 1,
+      email: formState.email,
+      password: formState.password,
+      tasks: [],
+    };
+    users.push(newUser);
+    ls("usersTaskHuman", JSON.stringify(users));
+    setDisabledLogin(true);
+    setMessage("User Created Successfully");
+    ls("loggedInUser", formState.email);
+    setTimeout(() => {
+      navigate("/tasks");
+    }, 500);
+  };
+
   return (
     <Container className=" d-flex flex-row justify-content-center">
       <div className="login-container">
-        <Form className="mx-auto">
+        <Form className="mx-auto" onSubmit={login}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              aria-label="email"
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) =>
+                setFormState({ ...formState, email: e.target.value })
+              }
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              aria-label="password"
+              type="password"
+              placeholder="Password"
+              onChange={(e) =>
+                setFormState({ ...formState, password: e.target.value })
+              }
+            />
           </Form.Group>
-          <Alert variant="primary">This is a primary alert—check it out!</Alert>
-          <Button variant="primary" type="submit">
+          {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="primary">{message}</Alert>}
+          <Button variant="primary" type="submit" disabled={disabledLogin}>
             Submit
           </Button>
         </Form>

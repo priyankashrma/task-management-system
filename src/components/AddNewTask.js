9b7ad0daa135266741ff073c1ccc092b77a50f70
Priyanka <priyankasharma.prisha@gmail.com>
2022-12-05 03:17:12 +0530

    addNewTask functionality

--- src/components/AddNewTask.js
+++ src/components/AddNewTask.js
@@ -1,19 +1,54 @@
-import React from "react";
+import ls from "local-storage";
+import React, { useContext, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { TaskContext } from "../context/TaskContext";
 
-const AddNewTask = () => {
+const AddNewTask = ({ tasks, setTasks }) => {
+  const today = new Date().toISOString().split("T")[0];
+
+  const { addTask } = useContext(TaskContext);
+
+  const [formState, setFormState] = useState({
+    title: "",
+    dueDate: "",
+  });
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    addTask(formState.title, formState.dueDate);
+    ls("tasks", tasks);
+    setFormState({ title: "", dueDate: today });
+  };
+
   return (
     <div className="new-task">
-      <Form.Label className="mb-1">Title</Form.Label>
-      <Form.Control aria-label="First name" />
+      <Form onSubmit={onFormSubmit}>
+        <Form.Label className="mb-1">Title</Form.Label>
+        <Form.Control
+          required
+          aria-label="Title"
+          name="name"
+          value={formState.title}
+          onChange={(e) =>
+            setFormState({ ...formState, title: e.target.value })
+          }
+        />
 
-      <Form.Label className="mb-1 mt-3">Due Date</Form.Label>
-      <Form.Control type="date" />
+        <Form.Label className="mb-1 mt-3">Due Date</Form.Label>
+        <Form.Control
+          type="date"
+          name="date"
+          value={formState.dueDate}
+          onChange={(e) =>
+            setFormState({ ...formState, dueDate: e.target.value })
+          }
+        />
 
-      <Button variant="primary" className="mt-3">
-        Add New Task
-      </Button>
+        <Button type="submit" variant="primary" className="mt-3">
+          Add Task
+        </Button>
+      </Form>
     </div>
   );
 };
